abaefe92fe9ae0c9e31975a4ae9dba54ddf8bf40
Bruno Lopes <44236749+o0brunolopes0o@users.noreply.github.com>
2024-09-24 10:23:25 -0300

    spring

---
+++ spring-ioc-di-beans-autowired/src/main/java/dio/primeiros_passos/PrimeirosPassosApplication.java
@@ -0,0 +1,26 @@
+package dio.primeiros_passos;
+
+import com.google.gson.Gson;
+import dio.primeiros_passos.app.ConversorJson;
+import dio.primeiros_passos.app.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
+
+}
